1324843eaf41f5517424d4aac7e4a070c1aa525d
růžový mravenečník Myšipaši <rostislav.kozlik@seznam.cz>
2026-09-15 18:40:21 +0200

    improvement

--- mrbeast/Program.cs
+++ mrbeast/Program.cs
@@ -1,15 +1,17 @@
-﻿class MrBeast {
+﻿using ChaChar = char;
+
+class MrBeast {
   public static void Main(String[] args) {
     int width = (int)Convert.ToInt64(Console.ReadLine());
     int height = (int)Convert.ToInt64(Console.ReadLine());
 
-    List<List<Char>> field = [];
+    List<List<ChaChar>> field = [];
 
     List<Int32> diddyblud = []; // x y dir
 
     for (int i = 0; i < height; i++) {
-      List<Char> row = (Console.ReadLine() ?? "").ToCharArray().ToList();
-      List<Char> bludi = row.FindAll((c) => "<>^v".Contains(c));
+      List<ChaChar> row = (Console.ReadLine() ?? "").ToCharArray().ToList();
+      List<ChaChar> bludi = row.FindAll((c) => "<>^v".Contains(c));
       if (bludi.Count() > 0) {
         diddyblud = [row.IndexOf(bludi.First()), i, ">^<v".IndexOf(bludi.First())];
         row[diddyblud[0]] = '.';
@@ -20,9 +22,9 @@
     List<List<Int32>> dirs = [[1, 0], [0, -1], [-1, 0], [0, 1]];
 
     for (int i = 0; i < 20; i++) {
-      Char front = field[diddyblud[1]+dirs[diddyblud[2]][1]][diddyblud[0]+dirs[diddyblud[2]][0]];
-      Char right = field[diddyblud[1]+dirs[(diddyblud[2]+3)%4][1]][diddyblud[0]+dirs[(diddyblud[2]+3)%4][0]];
-      Char frontright = field[diddyblud[1]+dirs[(diddyblud[2]+3)%4][1]+dirs[(diddyblud[2]+0)%4][1]][diddyblud[0]+dirs[(diddyblud[2]+3)%4][0]+dirs[(diddyblud[2]+0)%4][0]];
+      ChaChar front = field[diddyblud[1]+dirs[diddyblud[2]][1]][diddyblud[0]+dirs[diddyblud[2]][0]];
+      ChaChar right = field[diddyblud[1]+dirs[(diddyblud[2]+3)%4][1]][diddyblud[0]+dirs[(diddyblud[2]+3)%4][0]];
+      ChaChar frontright = field[diddyblud[1]+dirs[(diddyblud[2]+3)%4][1]+dirs[(diddyblud[2]+0)%4][1]][diddyblud[0]+dirs[(diddyblud[2]+3)%4][0]+dirs[(diddyblud[2]+0)%4][0]];
 
       if (front == 'X' && right == '.') diddyblud[2] = (diddyblud[2] + 3) % 4;
       if (front == '.' && right == '.') {
@@ -38,7 +40,7 @@
       }
 
       field[diddyblud[1]][diddyblud[0]] = ">^<v"[diddyblud[2]];
-      foreach (List<Char> row in field) Console.WriteLine(new String(row.ToArray()));
+      foreach (List<ChaChar> row in field) Console.WriteLine(new String(row.ToArray()));
       Console.WriteLine("\n");
       field[diddyblud[1]][diddyblud[0]] = '.';
     }
